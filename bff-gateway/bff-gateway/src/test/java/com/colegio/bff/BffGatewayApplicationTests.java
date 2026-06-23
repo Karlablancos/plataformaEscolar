cc87855720c8aef7f723d419c73b5aab092407cb
Katherine Cuestas <ka.cuestas@duocuc.cl>
2026-06-23 01:25:47 -0400

    test: agrega anotaciones @TestPropertySource en las pruebas de servicio

--- bff-gateway/bff-gateway/src/test/java/com/colegio/bff/BffGatewayApplicationTests.java
+++ bff-gateway/bff-gateway/src/test/java/com/colegio/bff/BffGatewayApplicationTests.java
@@ -2,8 +2,10 @@ package com.colegio.bff;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.jpa.hibernate.ddl-auto=none")
 class BffGatewayApplicationTests {
 
 	@Test
--- establecimiento/establecimiento/src/test/java/com/colegio/establecimiento/EstablecimientoApplicationTests.java
+++ establecimiento/establecimiento/src/test/java/com/colegio/establecimiento/EstablecimientoApplicationTests.java
@@ -2,8 +2,10 @@ package com.colegio.establecimiento;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.jpa.hibernate.ddl-auto=none")
 class EstablecimientoApplicationTests {
 
 	@Test
